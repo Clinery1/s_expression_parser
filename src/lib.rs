e1c65546c2b86dded911d29f40d84a124a4db532
Clinery1 <clinery8237@gmail.com>
2022-09-04 14:52:23 -0400

    Added a parse location to the objects and updated tests to reflect

--- src/lib.rs
+++ src/lib.rs
@@ -20,36 +20,43 @@ pub enum ObjectParseError {
 }
 #[derive(Debug,PartialEq,Clone)]
 pub enum Object<'input> {
-    List(Vec<Self>),
-    String(String),
-    Ident(&'input str),
-    Number(&'input str),
+    List(Location,Vec<Self>,Location),
+    String(Location,String,Location),
+    Ident(Location,&'input str,Location),
+    Number(Location,&'input str,Location),
 }
 impl<'input> Object<'input> {
     pub fn str_data(&self)->Option<&str> {
         match self {
-            Self::List(_)=>None,
-            Self::String(s)=>Some(&s),
-            Self::Ident(i)=>Some(i),
-            Self::Number(n)=>Some(n),
+            Self::List(..)=>None,
+            Self::String(_,s,_)=>Some(&s),
+            Self::Ident(_,i,_)=>Some(i),
+            Self::Number(_,n,_)=>Some(n),
         }
     }
     /// Parses the minimum amount of text to create an object.
     /// Returns the amount of characters used on success.
-    pub fn from_str_inner(s:&'input str,index_start:usize)->Result<(Self,usize),Error> {
+    fn from_str_inner(s:&'input str,location:&mut Location)->Result<(Self,usize),Error> {
         let mut indices=s.char_indices().peekable();
         let mut state;
         let mut start;
         let mut count=0;
         let mut in_comment=false;
         let mut negative=false;
+        let start_loc=*location;
         loop {
-            let (idx,c)=indices.next().ok_or(Error{index:index_start+count,err:NO_DATA})?;
+            let (idx,c)=indices.next().ok_or(Error{loc:*location,err:NO_DATA})?;
             count+=1;
             start=idx;
+            location.index+=1;
+            location.column+=1;
             if in_comment {
                 match c {
-                    '\n'=>in_comment=false,
+                    '\n'=>{
+                        in_comment=false;
+                        location.line+=1;
+                        location.column=0;
+                    },
                     _=>{},
                 }
             } else {
@@ -63,12 +70,16 @@ impl<'input> Object<'input> {
                         state=State::Number;
                         break;
                     },
-                    ' '|'\t'|'\r'|'\n'=>{},
+                    '\n'=>{
+                        location.line+=1;
+                        location.column=0;
+                    },
+                    ' '|'\t'|'\r'=>{},
                     '('=>{
                         state=State::List;
                         break;
                     },
-                    ')'=>return Err(Error{index:index_start+count,err:ObjectParseError::ExtraClosingParen}),
+                    ')'=>return Err(Error{loc:*location,err:ObjectParseError::ExtraClosingParen}),
                     ';'=>in_comment=true,
                     '"'=>{
                         state=State::String;
@@ -94,7 +105,7 @@ impl<'input> Object<'input> {
                                     state=State::Ident;
                                     continue;
                                 } else {
-                                    return Ok((Object::Number(&s[start..end]),count));
+                                    return Ok((Object::Number(start_loc,&s[start..end],*location),count));
                                 }
                             },
                             _=>{
@@ -109,9 +120,11 @@ impl<'input> Object<'input> {
                             '0'..='9'|'_'|'.'=>{},
                             _=>break,
                         }
+                        location.index+=1;
+                        location.column+=1;
                         count+=1;
                     }
-                    return Ok((Object::Number(&s[start..end]),count));
+                    return Ok((Object::Number(start_loc,&s[start..end],*location),count));
                 },
                 State::List=>{
                     let mut items=Vec::new();
@@ -121,11 +134,26 @@ impl<'input> Object<'input> {
                             ')'=>{
                                 good=true;
                                 count+=1;
+                                location.index+=1;
+                                location.column+=1;
                                 break;
                             },
-                            ' '|'\t'|'\r'|'\n'=>count+=1,
+                            '\n'=>{
+                                count+=1;
+                                location.line+=1;
+                                location.index+=1;
+                                location.column=0;
+                            },
+                            ' '|'\t'|'\r'=>{
+                                count+=1;
+                                location.index+=1;
+                                location.column+=1;
+                            },
                             _=>{
-                                let (obj,len)=Object::from_str_inner(&s[idx..],index_start+count)?;
+                                // eprintln!("List item before: {:?}",*location);
+                                // let item_start_idx=location.index;
+                                let (obj,len)=Object::from_str_inner(&s[idx..],location)?;
+                                // eprintln!("List item after: {:?}; Contents: `{}`",*location,&s[item_start_idx..location.index]);
                                 items.push(obj);
                                 count+=len;
                                 while let Some((idx,_))=indices.peek() {
@@ -139,9 +167,9 @@ impl<'input> Object<'input> {
                         }
                     }
                     if !good {
-                        return Err(Error{index:index_start+count,err:ObjectParseError::NoClosingParen});
+                        return Err(Error{loc:*location,err:ObjectParseError::NoClosingParen});
                     }
-                    return Ok((Object::List(items),count));
+                    return Ok((Object::List(start_loc,items,*location),count));
                 },
                 State::String=>{
                     let mut s=String::new();
@@ -149,6 +177,7 @@ impl<'input> Object<'input> {
                     let mut good=false;
                     for (_,c) in indices {
                         count+=1;
+                        let mut inc_col=true;
                         match c {
                             '\\'=>{
                                 if escape {
@@ -160,20 +189,31 @@ impl<'input> Object<'input> {
                             },
                             'n' if escape=>{s.push('\n');escape=false},
                             'r' if escape=>{s.push('\r');escape=false},
-                            '"' if escape=>{s.push('"');escape=false},
                             't' if escape=>{s.push('t');escape=false},
                             '0' if escape=>{s.push('\0');escape=false},
-                            '"' if !escape=>{
+                            '"'=>if escape {
+                                s.push('"');
+                                escape=false;
+                            } else {
                                 good=true;
+                                location.index+=1;
+                                location.column+=1;
                                 break;
+                            },
+                            '\n'=>{
+                                location.line+=1;
+                                location.column=0;
+                                inc_col=false;
                             },
                             _=>{s.push(c);escape=false},
                         }
+                        location.index+=1;
+                        if inc_col {location.column+=1}
                     }
                     if !good {
-                        return Err(Error{index:index_start+count,err:ObjectParseError::NoClosingParen});
+                        return Err(Error{loc:*location,err:ObjectParseError::NoClosingParen});
                     }
-                    return Ok((Object::String(s),count));
+                    return Ok((Object::String(start_loc,s,*location),count));
                 },
                 State::Ident=>{
                     let mut end=start;
@@ -184,51 +224,32 @@ impl<'input> Object<'input> {
                             _=>{},
                         }
                         count+=1;
+                        location.index+=1;
+                        location.column+=1;
                     }
-                    return Ok((Object::Ident(&s[start..end]),count));
+                    return Ok((Object::Ident(start_loc,&s[start..end],*location),count));
                 },
             }
         }
     }
     #[inline]
-    pub fn from_str(s:&'input str)->Result<(Self,usize),Error> {Self::from_str_inner(s,0)}
+    pub fn from_str(s:&'input str)->Result<(Self,usize),Error> {Self::from_str_inner(s,&mut Location::default())}
 }
 
 
 #[derive(Debug)]
 pub struct Error {
-    pub index:usize,
+    pub loc:Location,
     pub err:ObjectParseError,
 }
 impl Error {
-    pub fn line_col(&self,source:&str)->Option<(usize,usize)> {
-        if self.index>=source.len() {
-            return None;
-        }
-        let mut line=0;
-        let mut col=0;
-        for (idx,c) in source.char_indices() {
-            if idx>=self.index {
-                break;
-            }
-            col+=1;
-            match c {
-                '\n'=>{
-                    line+=1;
-                    col=0;
-                },
-                _=>{},
-            }
-        }
-        return Some((line,col));
-    }
     pub fn error_line<'a>(&self,source:&'a str)->Option<&'a str> {
-        if self.index>=source.len() {
+        if self.loc.index>=source.len() {
             return None;
         }
         let mut last_line=0;
         for (idx,c) in source.char_indices() {
-            if idx>=self.index {
+            if idx>=self.loc.index {
                 break;
             }
             match c {
@@ -238,7 +259,7 @@ impl Error {
                 _=>{},
             }
         }
-        let eol=source[self.index..].find('\n').unwrap_or(0)+self.index;
+        let eol=source[self.loc.index..].find('\n').unwrap_or(0)+self.loc.index;
         return Some(&source[last_line..eol].trim());
     }
 }
@@ -250,8 +271,9 @@ impl<'input> File<'input> {
     pub fn parse_file(contents:&'input str)->Result<Self,Error> {
         let mut items=Vec::new();
         let mut count=0;
+        let mut location=Location::default();
         while count<contents.len() {
-            match Object::from_str(&contents[count..]) {
+            match Object::from_str_inner(&contents[count..],&mut location) {
                 Ok((obj,len))=>{
                     items.push(obj);
                     count+=len;
@@ -267,4 +289,12 @@ impl<'input> File<'input> {
         }
         return Ok(File{items});
     }
+}
+#[derive(Copy,Clone,Debug,PartialEq,Default)]
+pub struct Location {
+    pub line:usize,
+    pub column:usize,
+    pub index:usize,
+}
+impl Location {
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,66 +1,90 @@
 use crate::*;
 
 
+const fn loc(line:usize,column:usize,index:usize)->Location {Location{line,column,index}}
 #[test]
 fn test_string() {
     let contents=r##""Hello world!""##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==14);
-    assert_eq!(obj,Object::String(String::from("Hello world!")));
+    assert_eq!(obj,Object::String(loc(0,0,0),String::from(&contents[1..13]),loc(0,14,14)));
 }
 #[test]
 fn test_number() {
     let contents=r##"1234 5678"##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==4);
-    assert_eq!(obj,Object::Number("1234"));
+    let start=Location{line:0,column:0,index:0};
+    let end=Location{line:0,column:4,index:4};
+    assert_eq!(obj,Object::Number(start,&contents[start.index..end.index],end));
 }
 #[test]
 fn test_ident() {
     let contents=r##"an_identifier another_one"##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==13);
-    assert_eq!(obj,Object::Ident("an_identifier"));
+    let start=Location{line:0,column:0,index:0};
+    let end=Location{line:0,column:13,index:13};
+    assert_eq!(obj,Object::Ident(start,&contents[start.index..end.index],end));
 }
 #[test]
 fn test_list1() {
     let contents=r##"(hello world)"##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==contents.len());
-    assert_eq!(obj,Object::List(vec![
-        Object::Ident("hello"),
-        Object::Ident("world"),
-    ]));
+    dbg!(&obj);
+    assert_eq!(obj,Object::List(Location{line:0,column:0,index:0},vec![
+        Object::Ident(Location{line:0,column:1,index:1},"hello",Location{line:0,column:6,index:6}),
+        Object::Ident(Location{line:0,column:7,index:7},"world",Location{line:0,column:12,index:12}),
+    ],Location{line:0,column:13,index:13}));
 }
 #[test]
 fn test_list2() {
     let contents=r##"(print "Hello, world!" 1234)"##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==contents.len());
-    assert_eq!(obj,Object::List(vec![
-        Object::Ident("print"),
-        Object::String("Hello, world!".to_string()),
-        Object::Number("1234"),
-    ]));
+    assert_eq!(obj,Object::List(loc(0,0,0),vec![
+        Object::Ident(loc(0,1,1),"print",loc(0,6,6)),
+        Object::String(loc(0,7,7),"Hello, world!".to_string(),loc(0,22,22)),
+        Object::Number(loc(0,23,23),"1234",loc(0,27,27)),
+    ],loc(0,28,28)));
 }
 #[test]
 fn test_list3() {
-    let contents=r##"(print (object (name "Clinery") (years_experience 6)) 1234)"##;
+    let contents=r##"(print
+    "Hello, world!")"##;
     let (obj,len)=Object::from_str(contents).unwrap();
     assert!(len==contents.len());
-    assert_eq!(obj,Object::List(vec![
-        Object::Ident("print"),
-        Object::List(vec![
-            Object::Ident("object"),
-            Object::List(vec![
-                Object::Ident("name"),
-                Object::String("Clinery".to_string()),
-            ]),
-            Object::List(vec![
-                Object::Ident("years_experience"),
-                Object::Number("6"),
-            ]),
-        ]),
-        Object::Number("1234"),
-    ]));
+    assert_eq!(obj,Object::List(loc(0,0,0),vec![
+        Object::Ident(loc(0,1,1),"print",loc(0,6,6)),
+        Object::String(loc(1,4,11),"Hello, world!".to_string(),loc(1,19,26)),
+    ],loc(1,20,27)));
+}
+#[test]
+fn test_list4() {
+    let contents=
+r##"(print
+    (object
+        (name "Clinery")
+        (years_experience 6))
+    1234)"##;
+    let (obj,len)=Object::from_str(contents).unwrap();
+    assert!(len==contents.len());
+    let to_match=Object::List(loc(0,0,0),vec![
+        Object::Ident(loc(0,1,1),"print",loc(0,6,6)),
+        Object::List(loc(1,4,11),vec![
+            Object::Ident(loc(1,5,12),"object",loc(1,11,18)),
+            Object::List(loc(2,8,27),vec![
+                Object::Ident(loc(2,9,28),"name",loc(2,13,32)),
+                Object::String(loc(2,14,33),"Clinery".to_string(),loc(2,23,42)),
+            ],loc(2,24,43)),
+            Object::List(loc(3,8,52),vec![
+                Object::Ident(loc(3,9,53),"years_experience",loc(3,25,69)),
+                Object::Number(loc(3,26,70),"6",loc(3,27,71)),
+            ],loc(3,28,72)),
+        ],loc(3,29,73)),
+        Object::Number(loc(4,4,78),"1234",loc(4,8,82)),
+    ],loc(4,9,83));
+    dbg!(&obj,&to_match);
+    assert_eq!(obj,to_match);
 }
